ad9851c70ccf732ff43ef38b204c483def619dea
Kartik <karkerakartik27@gmail.com>
2022-10-08 14:56:19 +0530

    Added age component

---
+++ src/components/Age/Age.js
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+import "./Age.css";
+
+function Age() {
+  useEffect(() => {
+    let ageEl = document.getElementById("age");
+    setInterval(() => {
+      let time =
+        (new Date() - new Date(998881445000)) / (1000 * 60 * 60 * 24 * 365.25); // milliseconds per year
+      ageEl.innerText = ` I am a ${time
+        .toString()
+        .substring(0, 12)} old developer.`;
+    }, 50);
+  }, []);
+
+  return (
+    <>
+      <div className="age" id="age">
+        21
+      </div>
+    </>
+  );
+}
+
+export default Age;
