96bf8b6b030dd3aee9280c77c6115c952ae529d6
Kartik <karkerakartik27@gmail.com>
2022-10-08 14:55:56 +0530

    Added name animation

---
+++ src/components/Typewriter/Typewriter.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Typewriter.css";
+
+function Typewriter() {
+  return (
+    <>
+      <h1 className="title">Kartik</h1>
+    </>
+  );
+}
+
+export default Typewriter;
