5d673ac347e3495dee4d8c8e4f1361e4c0e0037c
Kartik <karkerakartik27@gmail.com>
2022-10-08 14:58:37 +0530

    Added component for projects

---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,11 @@
+import "./Projects.css";
+
+function Projects() {
+  return (
+    <div className="projects">
+      I have made projects in Python and javascript.
+    </div>
+  );
+}
+
+export default Projects;
