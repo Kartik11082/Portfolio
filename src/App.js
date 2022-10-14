db3842a6911ed6966390c74d32e4cc3a3a604482
Kartik <karkerakartik27@gmail.com>
2022-10-14 21:46:01 +0530

    Added img folder

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import "./App.css";
-// import Typewriter from "./components/Typewriter/Typewriter";
-// import Age from "./components/Age/Age";
 import Home from "./components/Home/Home";
 
 function App() {
   return (
     <div className="App">
-      {/* <Typewriter />
-      <Age /> */}
       <Home />
     </div>
   );
