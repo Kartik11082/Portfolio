6f7b17b635b441968bd8ea8e0ac8b0e6d4fbce7f
Kartik <karkerakartik27@gmail.com>
2022-10-23 11:49:49 +0530

    Change to project layout

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,31 +1,50 @@
 import "./Projects.css";
 
 function Projects() {
-  const projectList = [
+  const pythonProjectList = [
     {
       title: "Attendance Manager",
-      description:
-        "Attendance Manager is a simple Python-powered application which helps teachers in managing online lecture attendance in a easier way. ",
-      image: "../../img/attendanceManager.jpg",
+      description: "Python application for managing online lecture attendance",
       url: "https://github.com/Kartik11082/Attendance-Manager",
     },
     {
       title: "Stacksearch",
       description: "CLI tool for searching in Stack Exchange ",
-      image: "https://picsum.photos/200",
       url: "https://github.com/Kartik11082/Stacksearch",
     },
     {
       title: "Student Management",
       description: "Project on Student Management System",
-      image: "https://picsum.photos/200",
       url: "https://github.com/Kartik11082/Student-Management",
     },
     {
       title: "Noting",
       description:
-        "Noting is a Web Application with User authentication for maintaining Notes/Tasks ",
-      image: "https://picsum.photos/200",
+        "Web Application with User authentication for maintaining notes",
+      url: "https://kartik110.pythonanywhere.com/",
+    },
+  ];
+
+  const projectList = [
+    {
+      title: "Attendance Manager",
+      description: "Python application for managing online lecture attendance",
+      url: "https://github.com/Kartik11082/Attendance-Manager",
+    },
+    {
+      title: "Stacksearch",
+      description: "CLI tool for searching in Stack Exchange ",
+      url: "https://github.com/Kartik11082/Stacksearch",
+    },
+    {
+      title: "Student Management",
+      description: "Project on Student Management System",
+      url: "https://github.com/Kartik11082/Student-Management",
+    },
+    {
+      title: "Noting",
+      description:
+        "Web Application with User authentication for maintaining notes",
       url: "https://kartik110.pythonanywhere.com/",
     },
   ];
@@ -37,18 +56,19 @@ function Projects() {
       </div>
       <div>
         <ul className="projectList">
-          {projectList.map((project) => {
+          {pythonProjectList.map((project) => {
             return (
-              <li className="project" key={project.title}>
-                <a href={project.url} target={"_blank"} rel="noreferrer">
+              <a
+                href={project.url}
+                target={"_blank"}
+                rel="noreferrer"
+                key={project.title}
+              >
+                <li className="project">
                   <h2>{project.title}</h2>
-                  {/* <p>{project.description}</p> */}
-                  <img
-                    src={require("../../img/attendanceManager.png")}
-                    alt={project.title}
-                  />
-                </a>
-              </li>
+                  <p>{project.description}</p>
+                </li>
+              </a>
             );
           })}
         </ul>
