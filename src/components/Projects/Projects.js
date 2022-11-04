ae641b8d6f9a0435d7f804e2b20e49ff3b0e02a6
Kartik <karkerakartik27@gmail.com>
2022-11-04 21:23:22 +0530

    Changed fonts

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,4 +1,7 @@
 import "./Projects.css";
+import uiPNG from "../../icons/ui.png";
+import wwwPNG from "../../icons/www.png";
+import terminalPNG from "../../icons/terminal.png";
 
 function Projects() {
   const pythonProjectList = [
@@ -6,22 +9,26 @@ function Projects() {
       title: "Attendance Manager",
       description: "Python application for managing online lecture attendance",
       url: "https://github.com/Kartik11082/Attendance-Manager",
+      icon: uiPNG,
     },
     {
       title: "Stacksearch",
       description: "CLI tool for searching in Stack Exchange ",
       url: "https://github.com/Kartik11082/Stacksearch",
+      icon: terminalPNG,
     },
     {
       title: "Student Management",
       description: "Project on Student Management System",
       url: "https://github.com/Kartik11082/Student-Management",
+      icon: uiPNG,
     },
     {
       title: "Noting",
       description:
         "Web Application with User authentication for maintaining notes",
       url: "https://kartik110.pythonanywhere.com/",
+      icon: wwwPNG,
     },
   ];
 
@@ -30,11 +37,13 @@ function Projects() {
       title: "Landing Page",
       description: "Website made in React ",
       url: "https://kcproject11.web.app/",
+      icon: wwwPNG,
     },
     {
       title: "Qontests",
       description: "React project using Kontests Api",
       url: "https://kontestsapi.web.app/",
+      icon: wwwPNG,
     },
   ];
 
@@ -71,6 +80,7 @@ function Projects() {
                 key={project.title}
               >
                 <li className="project">
+                  {/* <img src={project.icon} alt="icon" /> */}
                   <h2>{project.title}</h2>
                   <p>{project.description}</p>
                 </li>
@@ -90,6 +100,7 @@ function Projects() {
                 key={project.title}
               >
                 <li className="project">
+                  {/* <img src={project.icon} alt="icon" /> */}
                   <h2>{project.title}</h2>
                   <p>{project.description}</p>
                 </li>
