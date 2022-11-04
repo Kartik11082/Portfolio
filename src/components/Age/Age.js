9de0cc4bd1aac33ea8cc80d2239d727539e2b80b
Kartik <karkerakartik27@gmail.com>
2022-11-04 20:21:55 +0530

    Fixed typo in Age.js

--- src/components/Age/Age.js
+++ src/components/Age/Age.js
@@ -7,7 +7,7 @@ function Age() {
     setInterval(() => {
       let time =
         (new Date() - new Date(998881445000)) / (1000 * 60 * 60 * 24 * 365.25); // milliseconds per year
-      ageEl.innerText = ` I am a ${time.toString().substring(0, 12)} old`;
+      ageEl.innerText = ` I am a ${time.toString().substring(0, 12)} old programmer`;
     }, 50);
   }, []);
 
