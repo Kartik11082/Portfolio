990a1798c86da3ba9d9f45c0ecdedcaad3072142
Kartik11082 <karkerakartik27@gmail.com>
2025-12-16 10:44:56 -0600

    Pulled footer to top

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,24 @@
-import './App.css'
+import "./App.css";
 // import Title from components folder
-import Age from "./components/Age/Age"
-import Title from "./components/Title/Title"
+import Age from "./components/Age/Age";
+import Title from "./components/Title/Title";
 // import Projects from "./components/Projects/Projects"
-import Footer from "./components/Footer/Footer"
-import Projects from "./components/Projects/Projects"
-import Timeline from "./components/Timeline/Timeline"
-import Certification from "./components/Certification/Certification"
-
+import Footer from "./components/Footer/Footer";
+import Projects from "./components/Projects/Projects";
+import Timeline from "./components/Timeline/Timeline";
+import Certification from "./components/Certification/Certification";
 
 function App() {
-
   return (
     <>
       <Title />
       <Age />
+      <Footer />
       <Certification />
       <Projects />
       <Timeline />
-      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
